5c8129f44b6230754e563e72900f337f65b7c1b2
raghuvanshimanish657 <88127124+raghuvanshimanish657@users.noreply.github.com>
2022-09-23 17:28:10 +0530

    Time: 11 ms (50.93%), Space: 10 MB (63.15%) - LeetHub

--- 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
+++ 1464-maximum-product-of-two-elements-in-an-array/1464-maximum-product-of-two-elements-in-an-array.cpp
@@ -5,9 +5,9 @@ public:
         int max2=INT_MIN;
         for(int i:nums){
             if(i>=max1)max2=max1,max1=i;
-            cout<<"max1 : = "<<max1<<" , ";
+            //cout<<"max1 : = "<<max1<<" , ";
             if(i>=max2 && i<max1)max2=i;
-            cout<<"max2 : = "<<max2<<endl;
+            //cout<<"max2 : = "<<max2<<endl;
         }
         return (max1-1)*(max2-1);
     }
