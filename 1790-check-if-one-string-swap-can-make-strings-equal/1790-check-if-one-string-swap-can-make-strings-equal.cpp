ebc26e099b7dfc9b98060b2ff8d4a5bb288043ba
raghuvanshimanish657 <88127124+raghuvanshimanish657@users.noreply.github.com>
2022-09-24 14:37:32 +0530

    Time: 4 ms (40.25%), Space: 6.6 MB (5.51%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,6 +1,25 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
+
+        unordered_map<char,int>m1,m2;
+        int cnt=0;
+        for(int i=0;i<s1.size();i++){
+            if(s1[i]!=s2[i]){
+                cnt++;
+            }
+            m1[s1[i]]++; 
+        }
+        for(int i=0;i<s2.size();i++){
+           
+            m2[s2[i]]++; 
+        }
+        if((cnt==2 || s1==s2) && m1==m2){
+            return true;
+        }
+        return false;
+
+        /*
         if(s1.length()!=s2.length())
         return false;
     
@@ -20,5 +39,6 @@ public:
         }
 
         return true;
+        */
     }
 };
