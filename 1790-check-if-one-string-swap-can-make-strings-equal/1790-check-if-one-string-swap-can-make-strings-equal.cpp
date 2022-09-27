2cd30ddb3e4fc70105d736645d7eeab3aafa28d5
raghuvanshimanish657 <88127124+raghuvanshimanish657@users.noreply.github.com>
2022-09-27 13:51:59 +0530

    Time: 0 ms (100.00%), Space: 6.4 MB (8.15%) - LeetHub

--- 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
+++ 1790-check-if-one-string-swap-can-make-strings-equal/1790-check-if-one-string-swap-can-make-strings-equal.cpp
@@ -1,23 +1,44 @@
 class Solution {
 public:
     bool areAlmostEqual(string s1, string s2) {
-
-        unordered_map<char,int>m1,m2;
-        int cnt=0;
-        for(int i=0;i<s1.size();i++){
-            if(s1[i]!=s2[i]){
-                cnt++;
-            }
-            m1[s1[i]]++; 
-        }
-        for(int i=0;i<s2.size();i++){
-           
-            m2[s2[i]]++; 
-        }
-        if((cnt==2 || s1==s2) && m1==m2){
+        vector<int> add;
+        if (s1 == s2) {
             return true;
         }
+        if(s1.size()!=s2.size())return false;
+        for (int i = 0; i < s1.size(); i++) {
+            if (s1[i] == s2[i]) 
+                continue;
+            else {
+                add.push_back(i);
+            }
+        }
+        if (add.size() != 2) {
+            return false;
+        }
+        else {
+            swap (s1[add[0]], s1[add[1]]);
+            if (s2 == s1) return true;
+        }
         return false;
+    }
+        
+//         unordered_map<char,int>m1,m2;
+//         int cnt=0;
+//         for(int i=0;i<s1.size();i++){
+//             if(s1[i]!=s2[i]){
+//                 cnt++;
+//             }
+//             m1[s1[i]]++; 
+//         }
+//         for(int i=0;i<s2.size();i++){
+           
+//             m2[s2[i]]++; 
+//         }
+//         if((cnt==2 || s1==s2) && m1==m2){
+//             return true;
+//         }
+//         return false;
 
         /*
         if(s1.length()!=s2.length())
@@ -39,6 +60,6 @@ public:
         }
 
         return true;
-        */
-    }
+        
+    }*/
 };
