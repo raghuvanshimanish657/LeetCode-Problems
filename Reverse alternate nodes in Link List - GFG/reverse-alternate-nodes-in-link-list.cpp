fdba7bf70d92ab3f25e7e7800c70d8c3cdc7b0c4
raghuvanshimanish657 <88127124+raghuvanshimanish657@users.noreply.github.com>
2023-10-06 23:56:51 +0530

    Added solution - LeetHub

---
+++ Reverse alternate nodes in Link List - GFG/reverse-alternate-nodes-in-link-list.cpp
@@ -0,0 +1,134 @@
+//{ Driver Code Starts
+#include<stdio.h>
+#include<stdlib.h>
+#include<iostream>
+using namespace std;
+/* A linked list node */
+
+
+struct Node
+{
+    int data;
+    struct Node *next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+    
+};
+
+struct Node *start = NULL;
+
+/* Function to print nodes in a given linked list */
+void printList(struct Node *node)
+{
+    while (node != NULL)
+    {
+        printf("%d ", node->data);
+        node = node->next;
+    }
+    printf("\n");
+    
+}
+
+void insert()
+{
+    int n,value;
+    cin>>n;
+    struct Node *temp;
+    for(int i=0;i<n;i++)
+    {
+        cin>>value;
+        if(i==0)
+        {
+            start = new Node(value);
+            temp = start;
+            continue;
+        }
+        else
+        {
+            temp->next = new Node(value);
+            temp = temp->next;
+        }
+    }
+}
+
+
+// } Driver Code Ends
+/*
+  reverse alternate nodes and append at the end
+  The input list will have at least one element  
+  Node is defined as 
+  struct Node
+  {
+      int data;
+      struct Node *next;
+    
+      Node(int x){
+        data = x;
+        next = NULL;
+      }
+    
+   };
+
+*/
+class Solution
+{
+    public:
+    Node* reverseLL(struct Node* head){
+        struct Node* prev = NULL;
+        struct Node* curr = head;
+        
+        while(curr){
+            struct Node* nextNode = curr->next;
+            curr->next = prev;
+            prev = curr;
+            curr = nextNode;
+        }
+        
+        return prev;
+    }
+    
+    void rearrange(struct Node *odd)
+    {
+        if(odd == NULL || odd->next==NULL) return;
+        
+        struct Node* oddHead = odd;
+        struct Node* oddCurr = oddHead;
+        struct Node* evenHead = odd->next;
+        struct Node* evenCurr = evenHead;
+        
+        while(oddCurr && evenCurr && evenCurr->next){
+            oddCurr->next = evenCurr->next;
+            oddCurr = oddCurr->next;
+            evenCurr->next = oddCurr->next;
+            evenCurr = evenCurr->next;
+        }
+        
+        
+        oddCurr->next = reverseLL(evenHead);
+        // cout<<evenHead->data;
+        
+        return;
+        
+    }
+};
+
+
+
+//{ Driver Code Starts.
+int main()
+{
+    int t;
+    cin>>t;
+    while (t--) {
+        insert();
+        Solution ob;
+        ob.rearrange(start);
+        printList(start);
+    }
+    return 0;
+}
+
+// } Driver Code Ends
