5153dc450077abfbd8eb7a6fbffef4283ed031ed
raghuvanshimanish657 <88127124+raghuvanshimanish657@users.noreply.github.com>
2023-10-10 18:48:11 +0530

    Added solution - LeetHub

--- Nodes at given distance in binary tree - GFG/nodes-at-given-distance-in-binary-tree.cpp
+++ Nodes at given distance in binary tree - GFG/nodes-at-given-distance-in-binary-tree.cpp
@@ -96,63 +96,78 @@ struct Node
 */
 
 class Solution{
-  public:
-    Node* mark(unordered_map<Node *,Node *>& parent,Node *root,int target){
-        queue<Node *> q;
-        q.push(root);
-        Node *temp;
-        while(!q.empty()){
-            auto p = q.front(); q.pop();
-            if(p->data==target) temp=p;
-            if(p->left){
-                q.push(p->left);
-                parent[p->left]=p;
-            }
-            if(p->right){
-                q.push(p->right);
-                parent[p->right]=p;
-            }
+public:
+    void dfs(Node* root, unordered_map<Node*, Node*>& parent){
+        if(root == NULL) return;
+        if(root->left){
+            parent[root->left] = root;
+            dfs(root->left, parent);
         }
-        return temp;
-        
+        if(root->right){
+            parent[root->right] = root;
+            dfs(root->right, parent);
+        }
     }
-    vector <int> KDistanceNodes(Node* root, int target , int k)
-    {
-        unordered_map<Node *,Node *> parent;
-        Node* tar= mark(parent,root,target);
-        unordered_map<Node *,bool> visited;
-        queue<Node *> q;
-        q.push(tar);
-        visited[tar]=true;
-        int curr=0;
+    
+    void findNode(Node* root, Node* &targetNode, int t){
+        if(root == NULL) return;
+        if(root->data == t){
+            targetNode = root;
+            return;
+        }
+        if(root->left) findNode(root->left, targetNode, t);
+        if(root->right) findNode(root->right, targetNode, t);
+    }
+    
+    vector<int> KDistanceNodes(Node* root, int target, int k){
+        Node* targetNode;
+        findNode(root, targetNode, target);
+        
+        queue<Node*> q;
+        q.push(targetNode);
+        
+        unordered_map<Node*, Node*> parent;
+        parent[root] = NULL;
+        dfs(root, parent);
+        
+        unordered_map<Node*, bool> vis;
+        vis[targetNode] = true;
+        
+        int level = 0;
         while(!q.empty()){
-            int n = q.size();
-            if(curr++==k) break;
-            for(int i=0;i<n;i++){
-                auto p = q.front(); q.pop();
+            if(level == k) break;
+            level += 1;
             
-                if(p->left && !visited[p->left]){
-                    q.push(p->left);
-                    visited[p->left]=true;
+            int size = q.size();
+            
+            for(int i=1; i<=size; i++){
+                Node* node = q.front();
+                q.pop();
+                
+                if(node->left && !vis[node->left]){
+                    q.push(node->left);
+                    vis[node->left] = 1;
                 }
-                if(p->right && !visited[p->right]){
-                    q.push(p->right);
-                    visited[p->right]=true;
+                
+                if(node->right && !vis[node->right]){
+                    q.push(node->right);
+                    vis[node->right] = 1;
                 }
-                if(parent[p] && !visited[parent[p]]){
-                    q.push(parent[p]);
-                    visited[parent[p]]=true;
+                
+                if(parent[node] && !vis[parent[node]]){
+                    q.push(parent[node]);
+                    vis[parent[node]] = 1;
                 }
             }
-            
         }
-        vector <int> res;
+        
+        vector<int> res;
         while(!q.empty()){
-            auto p = q.front();
-            res.push_back(p->data);
+            res.push_back(q.front()->data);
             q.pop();
         }
-        sort(res.begin(),res.end());
+        sort(res.begin(), res.end());
+        
         return res;
     }
 };
